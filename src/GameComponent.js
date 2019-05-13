b42bbfa6205208c766374d5e762e816d975a6ebd
IPbiramontes <ibiramontes.student@hsfi.us>
2019-05-13 18:15:51 -0400

    Updating and interaction text
    Now when text is set to Firebase, it shows up on user's screens

--- src/GameComponent.js
+++ src/GameComponent.js
@@ -51,16 +51,20 @@ export default class GameComponent extends React.Component {
 
   /** Returns the current session ID as stored in Firebase.  */
   getSessionId() {
-    if (this.props &&
-        this.props.match &&
-        this.props.match.params &&
-        this.props.match.params.id) {
+    if (
+      this.props &&
+      this.props.match &&
+      this.props.match.params &&
+      this.props.match.params.id
+    ) {
       return this.props.match.params.id;
     }
-    if (this.props &&
-        this.props.location &&
-        this.props.location.state &&
-        this.props.location.state.id) {
+    if (
+      this.props &&
+      this.props.location &&
+      this.props.location.state &&
+      this.props.location.state.id
+    ) {
       return this.props.location.state.id;
     }
     throw new Error("Unable to get session ID from URL nor browser history");
@@ -68,15 +72,15 @@ export default class GameComponent extends React.Component {
 
   /** Returns the list of user IDs connected to the current session.  */
   getSessionUserIds() {
-    if (this.state &&
-        this.state.metadata &&
-        this.state.metadata.users) {
+    if (this.state && this.state.metadata && this.state.metadata.users) {
       return this.state.metadata.users;
     }
-    if (this.props &&
-        this.props.location &&
-        this.props.location.state &&
-        this.props.location.state.users) {
+    if (
+      this.props &&
+      this.props.location &&
+      this.props.location.state &&
+      this.props.location.state.users
+    ) {
       return this.props.location.state.users;
     }
     console.warn("Session user list not yet available, using empty list");
@@ -85,15 +89,15 @@ export default class GameComponent extends React.Component {
 
   /** Returns the user ID of the one who created this current session. */
   getSessionCreatorUserId() {
-    if (this.state &&
-        this.state.metadata &&
-        this.state.metadata.creator) {
+    if (this.state && this.state.metadata && this.state.metadata.creator) {
       return this.state.metadata.creator;
     }
-    if (this.props &&
-        this.props.location &&
-        this.props.location.state &&
-        this.props.location.state.creator) {
+    if (
+      this.props &&
+      this.props.location &&
+      this.props.location.state &&
+      this.props.location.state.creator
+    ) {
       return this.props.location.state.creator;
     }
     console.warn("Session creator ID not yet available, using empty string");
@@ -102,25 +106,27 @@ export default class GameComponent extends React.Component {
 
   /** Returns the session title, e.g., "Rock, Paper, Scissors" */
   getSessionTitle() {
-    if (this.state &&
-        this.state.metadata &&
-        this.state.metadata.title) {
+    if (this.state && this.state.metadata && this.state.metadata.title) {
       return this.state.metadata.title;
     }
-    if (this.props &&
-        this.props.location &&
-        this.props.location.state &&
-        this.props.location.state.title) {
+    if (
+      this.props &&
+      this.props.location &&
+      this.props.location.state &&
+      this.props.location.state.title
+    ) {
       return this.props.location.state.title;
     }
-    return "Studio Games!";  // Silently fall back to default value.
+    return "Studio Games!"; // Silently fall back to default value.
   }
 
   /** Returns the user ID of the current user, i.e. YOU. */
   getMyUserId() {
-    if (firebase.auth() &&
-        firebase.auth().currentUser &&
-        firebase.auth().currentUser.uid) {
+    if (
+      firebase.auth() &&
+      firebase.auth().currentUser &&
+      firebase.auth().currentUser.uid
+    ) {
       return firebase.auth().currentUser.uid;
     }
     throw new Error("Unable to get current user ID");
@@ -138,19 +144,14 @@ export default class GameComponent extends React.Component {
       if (data !== null) {
         let sessionMetadata = {
           creator: data.creator,
-          users: data.users,
-<<<<<<< HEAD
-          title: title
-        });
-=======
-        }
+          users: data.users
+        };
         if (data.type in gameData) {
           sessionMetadata.title = gameData[data.type].title;
         }
         let newState = this.state;
         newState.metadata = sessionMetadata;
         this.setState(newState);
->>>>>>> 3c580750cd951e2f9358879bd801375876db8338
         this.onSessionMetadataChanged(data);
       }
     });
--- src/games/teleAStory/teleAStory.js
+++ src/games/teleAStory/teleAStory.js
@@ -6,27 +6,46 @@ import UserApi from "../../UserApi.js";
 export default class TeleAStory extends GameComponent {
   constructor(props) {
     super(props);
-    this.getSessionDatabaseRef().set({ text: "Hello world" });
+    this.state = { textAreaVal: "", storyText: "" };
+    this.getSessionDatabaseRef().update({
+      text: "The Player's Story will display here"
+    });
   }
   onSessionDataChanged(data) {
     console.log("Data changed!", data);
+    this.setState({ storyText: data.text });
+  }
+  buttonHandler() {
+    this.getSessionDatabaseRef().update({ text: this.state.textAreaVal });
+  }
+  textAreaChangeHandler(event) {
+    console.log(event);
+    this.setState({ textAreaVal: event.target.value });
   }
   render() {
     var id = this.getSessionId();
     var users = this.getSessionUserIds().map(user_id => (
       <li key={user_id}>{UserApi.getName(user_id)}</li>
     ));
-    var creator = UserApi.getName(this.getSessionCreatorUserId());
+    var creator = UserApi.getName(event => this.getSessionCreatorUserId(event));
     return (
       <div>
         <p>Session ID: {id}</p>
         <p>Session creator: {creator}</p>
         <p>Session users:</p>
         <ul>{users}</ul>
-        <textarea rows="4" cols="50">
-          {" "}
+        <div>{this.state.storyText}</div>
+        <textarea
+          rows="4"
+          cols="50"
+          value={this.state.textAreaVal}
+          onChange={e => this.textAreaChangeHandler(e)}
+        >
+          <br />
         </textarea>
+        <button onClick={() => this.buttonHandler()}>cute little button</button>
       </div>
+      // textAreaVal
     );
   }
 }
