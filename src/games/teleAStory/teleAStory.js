702b03ddd851daa15dc3d6dea7ba89b8dee0dcee
ashleyd011901 <ashleyd0648@tywls.org>
2019-05-20 18:06:45 -0400

    we
    designed

--- src/games/teleAStory/teleAStory.js
+++ src/games/teleAStory/teleAStory.js
@@ -6,10 +6,54 @@ import UserApi from "../../UserApi.js";
 export default class TeleAStory extends GameComponent {
   constructor(props) {
     super(props);
-    this.getSessionDatabaseRef().set({});
+    this.state = {
+      currentText: " ",
+      displayText: " "
+    };
+    this.getSessionDatabaseRef().set({
+      current_player: UserApi.getName(this.getSessionCreatorUserId())
+    });
   }
+
   onSessionDataChanged(data) {
     console.log("Data changed!", data);
+    this.setState({ displayText: data.text });
+  }
+  handleButtonClick() {
+    var component = this;
+    this.getSessionDatabaseRef()
+      .once("value")
+      .then(function(snapshot) {
+        var fullstory = "";
+        if (snapshot.val() == null) {
+          fullstory = component.state.currentText;
+        } else {
+          fullstory = snapshot.val().text + "\n" + component.state.currentText;
+        }
+        component.getSessionDatabaseRef().update({
+          text: fullstory
+        });
+      });
+  }
+
+  // handleHostInit() {
+  //   const element = (
+  //     <div>
+  //       <textarea
+  //         rows="4"
+  //         cols="50"
+  //         onChange={event => this.handleChange(event)}
+  //         value={this.state.currentText}
+  //       />
+  //       <button onClick={() => this.handleButtonClick()}>
+  //         Click me, I'm a cute little button!
+  //       </button>
+  //     </div>
+  //   );
+  //   return element;
+  // }
+  handleChange(event) {
+    this.setState({ currentText: event.target.value });
   }
   render() {
     var id = this.getSessionId();
@@ -17,15 +61,40 @@ export default class TeleAStory extends GameComponent {
       <li key={user_id}>{UserApi.getName(user_id)}</li>
     ));
     var creator = UserApi.getName(this.getSessionCreatorUserId());
+
     return (
-      <div>
-        <p>Session ID: {id}</p>
-        <p>Session creator: {creator}</p>
-        <p>Session users:</p>
-        <ul>{users}</ul>
-        <textarea rows="4" cols="50">
-          {" "}
-        </textarea>
+      <div class="teleAStory">
+        {/* <p>Session ID: {id}</p> */}
+        <p class="teleAStoryText">Session creator: {creator}</p>
+        <p class="teleAStoryText">Session users:</p>
+        <ul class="teleAStoryText">{users}</ul>
+        <div class="storyTimeArea">
+          <textarea
+            readOnly
+            rows="10"
+            cols="50"
+            value={this.state.displayText}
+          />
+          <br />
+          <textarea
+            rows="4"
+            cols="50"
+            onChange={event => this.handleChange(event)}
+            value={this.state.currentText}
+          />
+          {/* <button onClick={() => this.handlehostInit()}>Start game</button> */}
+          <br />
+          <button
+            class="teleAStoryButton"
+            onClick={() => this.handleButtonClick()}
+          >
+            Submit
+            <br />
+            your
+            <br />
+            sentence!
+          </button>
+        </div>
       </div>
     );
   }
